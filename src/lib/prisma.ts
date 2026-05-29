266558b7d44a028221e4663746757d4a85660978
ciro pregot <ciropregot2006@gmail.com>
2026-05-29 17:50:43 -0300

    Simplificar inicializacion de Prisma Client

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,11 +1,6 @@
 import "dotenv/config";
 import { PrismaClient } from "@prisma/client";
-import { PrismaPg } from "@prisma/adapter-pg";
-import pg from "pg";
 
 console.log("Prisma Client inicializado. DATABASE_URL:", process.env.DATABASE_URL ? "SÍ" : "NO");
-const connectionString = process.env.DATABASE_URL;
-const pool = new pg.Pool({ connectionString });
-const adapter = new PrismaPg(pool);
 
-export const prisma = new PrismaClient({ adapter });
+export const prisma = new PrismaClient();
