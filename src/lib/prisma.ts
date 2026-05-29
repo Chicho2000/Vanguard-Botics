1b237b78d7faf8f6e0edd1d71fec1e38ab5b77aa
sofiakopr <sofiakoprcina@gmail.com>
2026-05-29 19:41:09 -0300

    Add files via upload

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,6 +1,15 @@
 import "dotenv/config";
 import { PrismaClient } from "@prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
+import { Pool } from "pg";
 
 console.log("Prisma Client inicializado. DATABASE_URL:", process.env.DATABASE_URL ? "SÍ" : "NO");
 
-export const prisma = new PrismaClient();
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: false,
+});
+const adapter = new PrismaPg(pool);
+
+export const prisma = new PrismaClient({ adapter });
+
